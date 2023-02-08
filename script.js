a9569513a547abdfcd8d057afb375d13e7041933
Abhishek Vedant <83869760+Webby1015@users.noreply.github.com>
2023-02-08 23:01:34 +0530

    more connections

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ let songinfo= document.getElementsByClassName('songInfo')
 let masterPlay = document.getElementById('MasterPlay')
 let forward = document.getElementById('forward')
 let backward = document.getElementById('backward')
-
+let songitem = document.getElementById('SongItems')
 let myprogressbar = document.getElementById('songprogress')
 
 let songs = [
-    {songname:"Dream on - Aerosmith",filepath:"sources/Songs/DreamOn.mp3",coverpath:"sources\\covers\\DreamOn.jpeg"},
-    {songname:"Good Riddance - Green Day",filepath:"sources\\Songs\\GoodRiddance.mp3",coverpath:"sources\\covers\\goodridance.jpeg"},
-    {songname:"Hotel California - Eagles",filepath:"sources\\Songs\\HotelCalifornia.mp3",coverpath:"sources\\covers\\hotel.jpeg"},
-    {songname:"Knockin on.. - Gun N' Roses",filepath:"sources\\Songs\\KnockinOnHeavensDoor.mp3",coverpath:"sources\\covers\\Knockin.jpeg"},
-    {songname:"Stairway To Heaven - Led Z",filepath:"sources\\Songs\\Stairway to Heaven.mp3",coverpath:"sources\\covers\\Stairway.jpeg"},
-    {songname:"Sweater Weather - The Neighbourhood",filepath:"sources\\Songs\\Sweater Weather.mp3",coverpath:"sources\\covers\\sweaterweather.jpeg"},
-    {songname:"Sweet Child O' Mine - Guns N' Roses",filepath:"sources\\Songs\\Sweet Child O' Mine.mp3",coverpath:"sources\\covers\\sweet.jpeg"}
+    {songname:"Dream on - Aerosmith",filepath:"sources/Songs/DreamOn.mp3",coverpath:"sources/covers/DreamOn.jpeg"},
+    {songname:"Good Riddance - Green Day",filepath:"sources/Songs/GoodRiddance.mp3",coverpath:"sources/covers/goodridance.jpeg"},
+    {songname:"Hotel California - Eagles",filepath:"sources/Songs/HotelCalifornia.mp3",coverpath:"sources/covers/hotel.jpeg"},
+    {songname:"Knockin on.. - Gun N' Roses",filepath:"sources/Songs/KnockinOnHeavensDoor.mp3",coverpath:"sources/covers/Knockin.jpeg"},
+    {songname:"Stairway To Heaven - Led Z",filepath:"sources/Songs/Stairway to Heaven.mp3",coverpath:"sources/covers/Stairway.jpeg"},
+    {songname:"Sweater Weather - The Neighbourhood",filepath:"sources/Songs/Sweater Weather.mp3",coverpath:"sources/covers/sweaterweather.jpeg"},
+    {songname:"Sweet Child O' Mine - Guns N' Roses",filepath:"sources/Songs/Sweet Child O' Mine.mp3",coverpath:"sources/covers/sweet.jpeg"}
 ]   
 
 let songpath = songs[songIndex]['filepath']
